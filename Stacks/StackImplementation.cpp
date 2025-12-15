195b204d5c4e991e6a6d43d5200865559e479337
vishal932 <vishaleverwin@gmail.com>
2025-12-15 15:11:20 +0530

    update

---
+++ Stacks/StackImplementation.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack {
+    int* arr;
+    int nextIndex;
+    int capacity;
+
+public:
+    Stack(int cap = 10) {
+        capacity = cap;
+        arr = new int[capacity];
+        nextIndex = 0;
+    }
+
+    int size() {
+        return nextIndex;
+    }
+
+    bool isEmpty() {
+        return nextIndex == 0;
+    }
+
+    void push(int val) {
+        if (nextIndex == capacity) {
+            cout << "Stack overflow\n";
+            return;
+        }
+        arr[nextIndex++] = val;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack underflow\n";
+            return;
+        }
+        nextIndex--;
+    }
+
+    int top() {
+        if (isEmpty()) {
+            cout << "Stack is empty\n";
+            return -1;
+        }
+        return arr[nextIndex - 1];
+    }
+
+    ~Stack() {
+        delete[] arr;
+    }
+};
+
+int main() {
+    Stack s(5);
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << s.top() << endl;   // 30
+    s.pop();
+    cout << s.top() << endl;   // 20
+
+    cout << s.size() << endl;  // 2
+
+    return 0;
+}
